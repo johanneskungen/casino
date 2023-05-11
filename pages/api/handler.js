20c3293858f41f59035f929cab638d61610d7dfa
Johannes Eriksson <johannes.einar.eriksson@gmail.com>
2023-05-11 16:06:31 +0200

    2

--- pages/api/handler.js
+++ pages/api/handler.js
@@ -1,4 +1,5 @@
-import prisma from "@/lib/prisma"
+import { PrismaClient } from "@prisma/client"
+const prisma = new PrismaClient()
 
 export default async function handle(req, res) {
     const rtps = await prisma.rtps.findMany()
